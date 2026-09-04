f650be5cda356d67d1db1590bdbbe8f7bc4c910a
GrupoTap <sousa.mecanica@gmail.com>
2026-09-04 19:04:17 -0300

    Add files via upload

--- field/sw.js
+++ field/sw.js
@@ -24,7 +24,7 @@
  *   abrindo a versão antiga por tempo indeterminado.
  * ========================================================================== */
 
-const VERSAO = 'S6';
+const VERSAO = 'S7';
 const CACHE  = 'tapflow-field-' + VERSAO;
 
 const ESSENCIAIS = [
